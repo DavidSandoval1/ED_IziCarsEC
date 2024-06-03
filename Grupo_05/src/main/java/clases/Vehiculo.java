94d56a2db5e2d42655c17c12e0ea23a0a955cba4
David Sandoval <150101008+DavidSandoval1@users.noreply.github.com>
2024-06-03 17:23:21 -0500

    Creacion de la clase Vehiculo

--- Grupo_05/src/main/java/clases/Vehiculo.java
+++ Grupo_05/src/main/java/clases/Vehiculo.java
@@ -4,10 +4,108 @@
  */
 package clases;
 
+import MyTDAs.LinkedListPRS;
+
 /**
  *
  * @author Pc
  */
 public class Vehiculo {
+    private String imagen;
+    private double precio;
+    private String marca;
+    private String modelo;
+    private int año;
+    private int kilometraje;
+    private String motor;
+    private String transmision;
+    private double peso;
+    private LinkedListPRS<String> historial;
+    
+    @Override
+    public String toString(){
+        return marca+" "+modelo;
+    }
+    
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public int getKilometraje() {
+        return kilometraje;
+    }
+
+    public void setKilometraje(int kilometraje) {
+        this.kilometraje = kilometraje;
+    }
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public void setMotor(String motor) {
+        this.motor = motor;
+    }
+
+    public String getTransmision() {
+        return transmision;
+    }
+
+    public void setTransmision(String transmision) {
+        this.transmision = transmision;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public LinkedListPRS<String> getHistorial() {
+        return historial;
+    }
+
+    public void setHistorial(LinkedListPRS<String> historial) {
+        this.historial = historial;
+    }
+    
     
 }
