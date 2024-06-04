b8999b95f5bfd7460ecbf45ee67f8e9cf7bc68b8
Jaren Pazmiño <jarapazm@espol.edu.ec>
2024-06-03 19:34:47 -0500

    Añadiendo Filtro por Año

---
+++ Grupo_05/src/main/java/clases/cmpAnio.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+import java.util.Comparator;
+
+/**
+ *
+ * @author jaren
+ */
+public class cmpAnio implements Comparator<Vehiculo>{
+    
+    @Override
+    public int compare(Vehiculo v1, Vehiculo v2)
+    {
+        return Integer.compare(v1.getAño(), v2.getAño());
+    }
+}
